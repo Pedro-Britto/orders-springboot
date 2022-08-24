d4aefd9a63873e581ad1b6ada1a95a7d247ece77
Pedro Britto <pedro7britto@gmail.com>
2022-08-23 22:26:52 -0300

    OrderStatus enum

--- src/main/java/com/example/orders/config/TestConfig.java
+++ src/main/java/com/example/orders/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.example.orders.config;
 
 import com.example.orders.entities.Order;
 import com.example.orders.entities.User;
+import com.example.orders.entities.enums.OrderStatus;
 import com.example.orders.repositories.OrderRepository;
 import com.example.orders.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED, u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
